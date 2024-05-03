1cf3debbc3ea444cf1957cb7ce114d40316339cc
Cameron Barr <cjbarr1013@gmail.com>
2024-05-03 19:40:46 -0400

    Integrated game functionality
    Updated game.js to work with updated interface. Also made some minor
    interface adjustments

--- game.js
+++ game.js
@@ -1,25 +1,31 @@
-function playGame() {
-    let i = playerScore = computerScore = 0;
-    
-    while (i < 5) {
-        const playerSelection = prompt('Rock, Paper, or Scissors?');
-        const computerSelection = getComputerChoice();
-        let roundResult = playRound(playerSelection, computerSelection);
-        if (roundResult === "w") {
-            playerScore++
-        } else if (roundResult === "l") {
-            computerScore++
-        } else {
-            continue
-        }
-        console.log(`Player: ${playerScore} || Computer: ${computerScore}`)
-        i++
-    }
+const playBtns = document.querySelectorAll(".play-btn");
+const output = document.querySelector("h2");
+const reset = document.querySelector(".reset-btn");
+let playerScore = document.querySelector("#player-score");
+let computerScore = document.querySelector("#cpu-score");
 
-    if (playerScore > computerScore) {
-        console.log(`Congrats! You won ${playerScore} to ${computerScore}!`)
-    } else {
-        console.log(`Sorry! You lost ${computerScore} to ${playerScore}!`)
+playBtns.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (playerScore.textContent < 5 && computerScore.textContent < 5) {
+            playGame(button.id);
+        }
+    })
+})
+
+reset.addEventListener("click", resetGame)
+
+function playGame(playerSelection) {
+
+    const computerSelection = getComputerChoice();
+    let roundResult = playRound(playerSelection, computerSelection);
+    if (roundResult === "w") {
+        playerScore.textContent++;
+    } else if (roundResult === "l") {
+        computerScore.textContent++;
+    } 
+
+    if (playerScore.textContent == 5 || computerScore.textContent == 5) {
+        endGame(playerScore, computerScore);
     }
 }
 
@@ -32,39 +38,49 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = capitalize(playerSelection)
     if (playerSelection === computerSelection) {
-        console.log(`You tied! ${playerSelection} ties ${computerSelection}!`);
+        output.textContent = `You tied! ${playerSelection} ties ${computerSelection}!`;
         return;
     } else if (playerSelection === "Rock") {
         if (computerSelection === "Paper") {
-            console.log("You lose! Paper covers Rock!");
+            output.textContent = "You lose! Paper covers Rock!";
             return "l";
         } else if (computerSelection === "Scissors") {
-            console.log("You win! Rock beats Scissors!");
+            output.textContent = "You win! Rock beats Scissors!";
             return "w";
         }
     } else if (playerSelection === "Paper") {
         if (computerSelection === "Scissors") {
-            console.log("You lose! Scissors cuts Paper!");
+            output.textContent = "You lose! Scissors cuts Paper!";
             return "l";
         } else if (computerSelection === "Rock") {
-            console.log("You win! Paper covers Rock!");
+            output.textContent = "You win! Paper covers Rock!";
             return "w";
         }
     } else if (playerSelection === "Scissors") {
         if (computerSelection === "Rock") {
-            console.log("You lose! Rock beats Scissors!");
+            output.textContent = "You lose! Rock beats Scissors!";
             return "l";
         } else if (computerSelection === "Paper") {
-            console.log("You win! Scissors cuts Paper!");
+            output.textContent = "You win! Scissors cuts Paper!";
             return "w";
         }
-    } else {
-        console.log("Please enter a valid input.")
-    }
+    } 
 }
 
 function capitalize(inputStr) {
     return inputStr[0].toUpperCase() + inputStr.slice(1).toLowerCase();
 }
 
-playGame()
+function endGame(playerScore, computerScore) {
+    if (playerScore.textContent > computerScore.textContent) {
+        output.textContent = `Congrats, you won! Click RESET to play again!`;
+    } else {
+        output.textContent = `Sorry, you lost! Click RESET to play again!`;
+    }
+}
+
+function resetGame() {
+    output.textContent = "Choose an option to start the game. First to 5.";
+    playerScore.textContent = 0;
+    computerScore.textContent = 0;
+}
